64f81188a1edc340469cb313152c8bc87091800e
barakroy@gmail.com <barakroy@gmail.com>
2018-03-13 01:09:00 +0200

    2nd

--- assets/js/script.js
+++ assets/js/script.js
@@ -49,43 +49,41 @@ function getAllImgs(search_val) {
 
 $('#search-btn').on('click', function () {
     var search_string = $('#img-search').val();
-    getAllImgs(search_string)
+    $('#myCarousel').hide();
+    $('#results').html('Here are the results for ' + '<span>'+search_string +'</span>' + '');
+    getAllImgs(search_string);
 });
 
+function reloadHome() {
+    location.reload();
+}
+function get_recent() {
+    $.get("https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=bcc7c7880769d72be1127fd7515bde3c&format=json&nojsoncallback=1", function (data) {
 
-//'<img src="http://farm'+data.photos.photo[0]['farm']+'.staticflickr.com/'+data.photos.photo[0]['id']+'/'+data.photos.photo[0]['server']+'_'+data.photos.photo[0]['secret']+'.jpg"'
-function get_recent(){
-    $.get( "https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=bcc7c7880769d72be1127fd7515bde3c&format=json&nojsoncallback=1", function( data ) {
-      
-    var active_item;
-    var cont;
-       $.each(data.photos.photo, function(index, element){
-           console.log(element)
-        if(index ===0){
-            active_item = 'active';
-            cont =` <div class="container">
+        var active_item;
+        var cont;
+        $.each(data.photos.photo, function (index, element) {
+
+            if (index === 0) {
+                active_item = 'active';
+                cont = ` <div class="container">
             <div class="carousel-caption text-left">
               <h1>Recent Images</h1>
               <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-            
             </div>
           </div>`;
-        } else {
-            active_item = '';
-        }
-        var template_carusel = `<div class="carousel-item ${active_item}">
+            } else {
+                active_item = '';
+            }
+            var template_carusel = `<div class="carousel-item ${active_item}">
         <img class="first-slide" src="http://farm${element.farm}.staticflickr.com/${element.server}/${element.id}_${element.secret}.jpg">
         ${cont}
       </div>`;
-      
-      $(template_carusel).appendTo(".carousel-inner");
-    })
-      });
 
-      //
-      //http://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}.jpg
+            $(template_carusel).appendTo(".carousel-inner");
+        })
+    });
+
+
 }
-get_recent()
-//http://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}.jpg
-//http://farm5.staticflickr.com/40775781451/4796_a21b72809c.jpg 502 (Cannot find server.)
-//http://farm5.staticflickr.com/4775/25904526807_308013c0df.jpg
+get_recent();
